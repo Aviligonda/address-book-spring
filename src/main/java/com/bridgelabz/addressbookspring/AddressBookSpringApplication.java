09285d72fdb43c0c4f849be0b5aceaf3d4744b1b
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-10 15:30:11 +0530

    ADDED : Welcome message

--- src/main/java/com/bridgelabz/addressbookspring/AddressBookSpringApplication.java
+++ src/main/java/com/bridgelabz/addressbookspring/AddressBookSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookSpringApplication {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Spring AddressBook  Program");
         SpringApplication.run(AddressBookSpringApplication.class, args);
     }
 
