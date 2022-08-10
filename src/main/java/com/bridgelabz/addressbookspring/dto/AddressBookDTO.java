758946a689136a179189758a9e212588eda7ee52
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-10 15:53:12 +0530

    ADDED : adding one jpa dependncy and creating model and dto classes

---
+++ src/main/java/com/bridgelabz/addressbookspring/dto/AddressBookDTO.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookspring.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long phoneNumber;
+    private int zip;
+}
